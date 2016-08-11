7cd0ae65607708002f4d73dd59ffe10038c570f5
rijuvenator <jujudg1031@gmail.com>
2016-08-11 16:59:39 +0200

    added L1A column from TMB dumps

--- TestBeamAnalysis/database_scraper/trig.sh
+++ TestBeamAnalysis/database_scraper/trig.sh
@@ -17,7 +17,7 @@ do
 		else
 			print $2
 		}
-		/13CLCT|14CLCT|15CLCT|16CLCT|17CLCT|18CLCT|19CLCT|20CLCT|29CLCT|30CLCT|32TMB/ {
+		/13CLCT|14CLCT|15CLCT|16CLCT|17CLCT|18CLCT|19CLCT|20CLCT|29CLCT|30CLCT|32TMB|55L1A/ {
 		print $NF
 		}
 		' $i)"
@@ -32,7 +32,7 @@ do
 		else
 			print $2
 		}
-		/13CLCT|14CLCT|15CLCT|16CLCT|17CLCT|18CLCT|19CLCT|20CLCT|27CLCT|28CLCT|30TMB/ {
+		/13CLCT|14CLCT|15CLCT|16CLCT|17CLCT|18CLCT|19CLCT|20CLCT|27CLCT|28CLCT|30TMB|53L1A/ {
 		print $NF
 		}
 		' $i)"
@@ -41,9 +41,9 @@ do
 done
 awk '
 	BEGIN {
-			printf "%4s %12s %12s %12s %12s %12s %12s %12s %12s %12s %12s %12s\n", "MEAS", "Total", "CFEB0", "CFEB1", "CFEB2", "CFEB3", "CFEB4", "CFEB5", "CFEB6", "CLCT0", "CLCT1", "ALCT*CLCT"
+			printf "%4s %12s %12s %12s %12s %12s %12s %12s %12s %12s %12s %12s %5s\n", "MEAS", "Total", "CFEB0", "CFEB1", "CFEB2", "CFEB3", "CFEB4", "CFEB5", "CFEB6", "CLCT0", "CLCT1", "ALCT*CLCT", "L1A"
 		}
 	{
-		printf "%4i %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f\n",  $1, $2/$NF, $3/$NF, $4/$NF, $5/$NF, $6/$NF, $7/$NF, $8/$NF, $9/$NF, $10/$NF, $11/$NF, $12/$NF
+		printf "%4i %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %12.4f %5i\n",  $1, $2/$NF, $3/$NF, $4/$NF, $5/$NF, $6/$NF, $7/$NF, $8/$NF, $9/$NF, $10/$NF, $11/$NF, $12/$NF, $13
 	}
 	' trigdata > tmp; mv tmp trigdata
