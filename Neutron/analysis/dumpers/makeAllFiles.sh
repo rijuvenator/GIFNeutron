132b13717b1ff9a46df7e08f8c9d90078aecd008
rijuvenator <jujudg1031@gmail.com>
2016-09-07 11:44:26 +0200

    added ZA plot and satellite files

--- Neutron/analysis/dumpers/makeAllFiles.sh
+++ Neutron/analysis/dumpers/makeAllFiles.sh
@@ -12,6 +12,7 @@ python makeSpectrum.py           $SUFFIX > ../files/energies_${SUFFIX}
 python makeDist.py               $SUFFIX > ../files/dists_${SUFFIX}
 python makePhotonEnergies.py     $SUFFIX > ../files/photons_${SUFFIX}
 python makePosition.py           $SUFFIX > ../files/pos_final_${SUFFIX}
+python makeNuclei.py             $SUFFIX > ../files/nuclei_${SUFFIX}
 
 # Still needs some work
 #python makeThermalPathLengths.py $SUFFIX > ../files/paths_${SUFFIX}
