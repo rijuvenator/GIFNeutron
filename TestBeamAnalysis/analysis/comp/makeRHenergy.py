7071e16ea6392cba7ae3780aefd9c50f0d55cb03
Christian <c.schnaible@cern.ch>
2016-12-13 12:05:41 +0100

    add LCT pattern requirement to makeRHenergy

--- TestBeamAnalysis/analysis/comp/makeRHenergy.py
+++ TestBeamAnalysis/analysis/comp/makeRHenergy.py
@@ -109,6 +109,7 @@ class MegaStruct():
 										if comp.layer!=rechits[rhID].layer: continue
 										if not self.matchRHComp(rechits[rhID],comp): continue
 										if c in alreadyMatched: continue
+										if not self.inLCTPattern(lct,comp): continue
 										alreadyMatched.append(c)
 										matched = True
 										break
@@ -172,6 +173,36 @@ class MegaStruct():
 		if diff<=2:
 			return True
 		else:
+			return False
+
+	# check if comparator is in LCT pattern
+	def inLCTPattern(self,lct,comp):
+		id_ = lct.pattern
+		# lct khs is 0 indexed, comp hs is 1 indexed
+		khs = lct.keyHalfStrip+2
+
+		if id_ == 2:
+			pat = {6:[khs-5, khs-4, khs-3], 5:[khs-4, khs-3, khs-2], 4:[khs-2, khs-1, khs], 3:[khs], 2:[khs+1, khs+2], 1:[khs+3, khs+4, khs+5]}
+		elif id_ == 3:
+			pat = {1:[khs-5, khs-4, khs-3], 2:[khs-2, khs-1], 3:[khs], 4:[khs, khs+1, khs+2], 5:[khs+2, khs+3, khs+4], 6:[khs+3, khs+4, khs+5]}
+		elif id_ == 4:
+			pat = {6:[khs-4, khs-3, khs-2], 5:[khs-4, khs-3, khs-2], 4:[khs-2, khs-1], 3:[khs], 2:[khs+1, khs+2], 1:[khs+2, khs+3, khs+4]}
+		elif id_ == 5:
+			pat = {1:[khs-4, khs-3, khs-2], 2:[khs-2, khs-1], 3:[khs], 4:[khs+1, khs+2], 5:[khs+2, khs+3, khs+4], 6:[khs+2, khs+3, khs+4]}
+		elif id_ == 6:
+			pat = {6:[khs-3, khs-2, khs-1], 5:[khs-2, khs-1], 4:[khs-1, khs], 3:[khs], 2:[khs, khs+1], 1:[khs+1, khs+2, khs+3]}
+		elif id_ == 7:
+			pat = {1:[khs-3, khs-2, khs-1], 2:[khs-1, khs], 3:[khs], 4:[khs, khs+1], 5:[khs+1, khs+2], 6:[khs+1, khs+2, khs+3]}
+		elif id_ == 8:
+			pat = {6:[khs-2, khs-1, khs], 5:[khs-2, khs-1, khs], 4:[khs-1, khs], 3:[khs], 2:[khs, khs+1], 1:[khs, khs+1, khs+2]}
+		elif id_ == 9:
+			pat = {1:[khs-2, khs-1, khs], 2:[khs-1, khs], 3:[khs], 4:[khs, khs+1], 5:[khs, khs+1, khs+2], 6:[khs, khs+1, khs+2]}
+		elif id_ == 10:
+			pat = {6:[khs-1, khs, khs+1], 5:[khs-1, khs, khs+1], 4:[khs], 3:[khs], 2:[khs], 1:[khs-1, khs, khs+1]}
+
+		if comp.halfStrip in pat[comp.layer]:
+			return True
+		else: 
 			return False
 
 	# get a current measurement given a chamber and measurement number
